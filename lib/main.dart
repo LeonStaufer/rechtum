b31de97dfc9c9a85e1e330df4ebf3664011c79f0
Leon Staufer <leon@staufer.me>
2021-04-16 20:51:10 +0200

    add open sans font and colors

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:openai_gpt3_api/openai_gpt3_api.dart';
 import 'package:provider/provider.dart';
 import 'package:rechtum/services/debug_logger.dart';
 import 'package:rechtum/services/locator.dart';
-import 'package:rechtum/utils/color.dart';
 import 'package:rechtum/view_models/chat_view_model.dart';
 import 'package:rechtum/views/chat_page.dart';
 import 'package:rechtum/views/home_page.dart';
@@ -19,8 +18,19 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData(
-      fontFamily: "IBM Plex Sans",
-      primarySwatch: createMaterialColor(Color(0xff0065bd)),
+      fontFamily: "Opens Sans",
+      primaryColorDark: Color(0xff0f3750),
+      primaryColor: Color(0xff0076b8),
+      primaryColorLight: Color(0xff53a7d5),
+      accentColor: Color(0xff51a4d1),
+      textTheme: TextTheme(
+        headline1: TextStyle(fontFamily: "IBM Plex Sans"),
+        headline2: TextStyle(fontFamily: "IBM Plex Sans"),
+        headline3: TextStyle(fontFamily: "IBM Plex Sans"),
+        headline4: TextStyle(fontFamily: "IBM Plex Sans"),
+        headline5: TextStyle(fontFamily: "IBM Plex Sans"),
+        headline6: TextStyle(fontFamily: "IBM Plex Sans"),
+      ),
     );
 
     return MaterialApp(
